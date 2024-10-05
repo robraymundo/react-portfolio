bcd7739cc145f8ce51c7193acaa1cd636c740ed4
robraymundo <rbnr19@gmail.com>
2024-10-05 22:49:49 +0800

    Update title in landing page

--- src/components/LandingSection/LandingSection.jsx
+++ src/components/LandingSection/LandingSection.jsx
@@ -2,12 +2,12 @@ import "./LandingSection.css";
 
 function Header() {
   const Logo = "/profile_picture.jpg";
-  
+
   return (
-    <header className="header">
+    <header className="header" id="home">
       <img src={Logo} alt="profile" className="header__image"/>
       <h2 className="header__name">Hello, Im Robin!</h2>
-      <h1 className="header__title">A frontend developer <br/> specializing in react</h1>
+      <h1 className="header__title">Aspiring Software Engineer specializing <br/> in web and app development</h1>
     </header>
   )
 }
