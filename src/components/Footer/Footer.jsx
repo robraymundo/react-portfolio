3527db5481436c2c89b4148d2fff0d0fd6e42d55
robraymundo <rbnr19@gmail.com>
2024-10-05 22:01:12 +0800

    Change the copyright statement

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer>
-      <p>Robin Raymundo © 2024</p>
+      <p>Robin Raymundo © 2024 - All rights reserved.</p>
     </footer>
   )
 }
