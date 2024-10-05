6a209cb902ea84c647e577126bd451fe1496bbfa
robraymundo <rbnr19@gmail.com>
2024-10-05 22:07:44 +0800

    Implement hide/show animation for nav bar on scroll

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,40 +4,60 @@ import { faFacebook, faGit } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { useState, useEffect } from 'react';
 
 function NavBar() {
+  const [showNav, setShowNav] = useState(true);
+  let lastScrollY = window.scrollY;
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      setShowNav(false);
+    } else {
+      setShowNav(true);
+    }
+    lastScrollY = window.scrollY;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="nav-bar">
+    <nav className={`nav-bar ${showNav ? 'nav-bar--visible' : 'nav-bar--hidden'}`}>
       <ul className="nav-bar__list">
         <li className="nav-bar__item">
           <a className="nav-bar__link" href="#">Home</a>
         </li>
         <li className="nav-bar__item">
-          <a className="nav-bar__link" href="#">Projects</a>
+          <a className="nav-bar__link" href="#projects">Projects</a>
         </li>
         <li className="nav-bar__item">
-          <a className="nav-bar__link" href="#">Contact Me</a>
+          <a className="nav-bar__link" href="#contact">Contact Me</a>
         </li>
       </ul>
 
       <ul className="nav-bar__socials">
         <li className="nav-bar__social-item">
-          <a className="nav-bar__link" href="#">
+          <a className="nav-bar__link" href="https://www.facebook.com/Robbbnn" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFacebook} size="xl" inverse/>
           </a>
         </li>
         <li className="nav-bar__social-item">
-          <a className="nav-bar__link" href="#">
+          <a className="nav-bar__link" href="https://github.com/robraymundo" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} size="xl" inverse/>
           </a>
         </li>
         <li className="nav-bar__social-item">
-          <a className="nav-bar__link" href="#">
+          <a className="nav-bar__link" href="https://www.linkedin.com/in/robin-raymundo-bb6003271/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} size="xl" inverse/>
           </a>
         </li>
         <li className="nav-bar__social-item">
-          <a className="nav-bar__link" href="#">
+          <a className="nav-bar__link" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faInstagram} size="xl" inverse/>
           </a>
         </li>
