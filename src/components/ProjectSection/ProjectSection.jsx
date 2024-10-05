928da47b24b8a9c080f8ac4c058f8510adc8b7be
robraymundo <rbnr19@gmail.com>
2024-10-05 22:10:05 +0800

    Initialize my projects

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -5,38 +5,42 @@ import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 function ProjectSection() {
   const projects = [
     {
-      imgSrc: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-      title: "React Calculator",
-      description: "A simple calculator built using React."
+      title: "Calculator",
+      description: "A simple calculator app built with Python's Tkinter library. It supports basic arithmetic operations. ",
+      imageSource: "/thumbnails/calculator.jpg", 
+      link: "https://github.com/robraymundo/calculator"
     },
     {
-      imgSrc: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-      title: "React Calculator",
-      description: "A simple calculator built using React."
+      title: "Dividers",
+      description: "A simulation game where you can spawn a paper, rock or scissor. The last standing team wins.",
+      imageSource: "/thumbnails/dividers.png",
+      link: "https://github.com/robraymundo/dividers"
     },
     {
-      imgSrc: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-      title: "React Calculator",
-      description: "A simple calculator built using React."
+      title: "Music Player",
+      description: "Simple music player created in Python with Tkinter library that offers simple features like play, pause and stop.",
+      imageSource: "/thumbnails/music_player.jpg",
+      link: "https://github.com/robraymundo/music-player"
     },
     {
-      imgSrc: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-      title: "React Calculator",
-      description: "A simple calculator built using React."
+      title: "PyDino",
+      description: "My very first Python project, a simple game where the player controls a dinosaur to avoid obstacles",
+      imageSource: "/thumbnails/pydino.jpg",
+      link: "https://github.com/robraymundo/pydino"
     }
   ];
 
   return (
-    <div className="project-section">
+    <div className="project-section" id="projects">
       <h1 className="project-section__heading">Projects</h1>
       <div className="project-section__content">
         {projects.map((project, index) => (
           <div className="project-section__item" key={index}>
-            <img className="project-section__image" src={project.imgSrc} alt="" />
+            <img className="project-section__image" src={project.imageSource} alt="" />
             <div className="project-section__text">
-              <h1 className="project-section__title">{project.title}</h1>
+              <h2 className="project-section__title">{project.title}</h2>
               <p className="project-section__description">{project.description}</p>
-              <a className="project-section__link" href="#">
+              <a className="project-section__link" href={project.link} target="_blank" rel="noopener noreferrer">
                 <p>See more</p>
                 <FontAwesomeIcon icon={ faArrowRightLong } size="lg"/>
               </a>
